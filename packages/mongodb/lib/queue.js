f1b334fd561a11bf1d3a30d65bd37b4406285d20
Garrick Meeker <gmeeker@gmail.com>
2018-10-19 20:04:10 -0700

    Avoid deprecated mongodb count()

--- packages/mongodb/lib/queue.js
+++ packages/mongodb/lib/queue.js
@@ -1,6 +1,10 @@
 const mongoDbQueue = require('mongodb-queue');
 const { promisify } = require('util');
 
+function now() {
+  return (new Date()).toISOString();
+}
+
 class Queue extends mongoDbQueue {
   constructor(db, name, options) {
     let o = options;
@@ -19,6 +23,42 @@ class Queue extends mongoDbQueue {
     this.done = promisify(this.done);
     this.clean = promisify(this.clean);
     this.createIndexes = promisify(this.createIndexes);
+  }
+
+  newCount(query, callback) {
+    this.col.countDocuments(query, function (err, count) {
+      if (err) return callback(err);
+      callback(null, count);
+    });
+  }
+
+  // Avoid deprecated mongodb commands
+  total(callback) {
+    this.newCount({}, callback);
+  }
+
+  size(callback) {
+    const query = {
+      deleted: null,
+      visible: { $lte: now() },
+    };
+    this.newCount(query, callback);
+  }
+
+  inFlight(callback) {
+    const query = {
+      ack: { $exists: true },
+      visible: { $gt: now() },
+      deleted: null,
+    };
+    this.newCount(query, callback);
+  }
+
+  done(callback) {
+    const query = {
+      deleted: { $exists: true },
+    };
+    this.newCount(query, callback);
   }
 }
 
